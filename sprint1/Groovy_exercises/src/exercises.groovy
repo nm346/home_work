9ac95316f4a53f665867be13e3d471d01f0a6169
nm346 <nm346@student.le.ac.uk>
2016-10-05 22:04:30 +0100

    Update exercises.groovy

--- sprint1/Groovy_exercises/src/exercises.groovy
+++ sprint1/Groovy_exercises/src/exercises.groovy
@@ -3,15 +3,19 @@ def date = new Date().format ("dd-mm-yyyy")
 def fruitBag = ["orange","banana","coconut"]
 
 // ex 01 (*). print "my name: 06-18-2016" on the output console using a GString
-
+println "$name: 06-18-2016" 
 // ex 02 (*). use a loop for printing the members of 
 // the collection fruitBag
-
+for(fruit in ["orange","banana","coconut"]){
+  println fruit
+}
 // ex 03 (*). define a function that given a range of integers, 
 // determines the average
 
+
 // ex 04 (**). using a closure check whether fruitBag 
 // contains 'banana' 
+  ["orange","banana","coconut"].find{it == "banana"}
 
 // ex 05 (**). using a closure check whether all the words
 // in fruitBag contain the letter 'n'
